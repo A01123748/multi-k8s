cc09ac23f6b20c1a6d9f2d146669376cc2250df3
Eliseo Fuentes <eliseo.fuentes@perficient.com>
2021-03-18 16:49:00 -0600

    Initial commit

--- client/src/App.js
+++ client/src/App.js
@@ -10,17 +10,18 @@ function App() {
     <Router>
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <a
+        <a
             className="App-link"
             href="https://reactjs.org"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Fib Calculator
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Fib Calculator version 2</h1>
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
+         
         </header>
         <div>
           <Route exact path="/" component={Fib} />
